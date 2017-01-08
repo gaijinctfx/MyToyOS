3aec91a55873cbf104d290b0e9a2fe2f31ffed4d
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2017-01-08 14:06:11 -0200

    Fixed.

--- src/boot/tests/dos/RD_GEOM.C
+++ src/boot/tests/dos/RD_GEOM.C
@@ -4,31 +4,43 @@
 
 // Disk Access Paramater Table
 struct dap_s {
-  unsigned short size;
-  unsigned short count;
-  unsigned short ofs, seg;
+  unsigned int size;
+  unsigned int count;
+  unsigned int ofs, seg;
   unsigned long  lba_lo, lba_hi;
 };
 
 // Partition Table Entry.
 struct geom_s {
+  unsigned char status;
+  unsigned char start_chs[3];
+  unsigned char type;
+  unsigned char end_chs[3];
+  unsigned long lba;          // only lba28 supported?
+  unsigned long sectors;
 };
 
-int get_disk_geometry(unsigned char, struct geom_s *);
+int get_disk_geometry(unsigned char, void *);
+
+unsigned char buffer[512];
 
 int main(void)
 {
-  if (get_disk_geometry(0x80, &g))
+  struct geom_s *p;
+
+  if (get_disk_sector(0x80, buffer))
   {
     fpurs("Error reading MBR!\n", stderr);
     return 1;
   }
 
-  // Show partition table entry.
+  p = (struct geom_s *)(((char *)buffer)+0x17e);
 
+  // Show partition table entry.
+  printf("LBA: %lu, sectors: %lu\n", p->lba, p->sectors);
 }
 
-int get_disk_geometry(unsigned char drive, struct geom_s *g)
+int get_disk_sector(unsigned char drive, void *buffer)
 {
   struct dap_s dap;
   union REGS regs;
@@ -36,13 +48,14 @@ int get_disk_geometry(unsigned char drive, struct geom_s *g)
 
   dap.size = sizeof(struct dap_s);
   dap.count = 1;
-  dap.ofs = FP_OFF(g);
-  dap.seg = FP_SEG(g);
+  dap.ofs = FP_OFF(buffer);
+  dap.seg = FP_SEG(buffer);
   dap.lba_lo = dap.lba_hi = 0;  // mbr.
 
   regs.h.ah = 0x42;
-  regs.x.bx = FP_OFF(&dap);
-  sregs.es = FP_SEG(&dap);
+  regs.h.dl = drive;
+  regs.x.si = FP_OFF(&dap);
+  sregs.ds = FP_SEG(&dap);
   int86x(0x13, &regs, &regs, &sregs);
   return !!regs.cflag;
 }
