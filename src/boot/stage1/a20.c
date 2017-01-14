4e870c28c5a780a0f4b8131c2b238765dc773c2d
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2017-01-14 07:53:31 -0200

    A little improvement on gate A20 test routine. Changed the a20 enabling mechanism to try int 0x15, kbdc method and, last resort, fast method. FIXME: Is this ok?

--- src/boot/stage1/a20.c
+++ src/boot/stage1/a20.c
@@ -3,8 +3,8 @@
 #include <hw_io.h>
 #include <macros.h>
 
-static void enable_a20_fast(void) { outpb(0x92, (inpb(0x92) | 0x02) & ~1); }
-static void wait_kbdc(void) { while (inpb(0x64) & 2) { io_delay(); } }
+static void enable_a20_fast(void)   { outpb(0x92, (inpb(0x92) | 0x02) & ~1); }
+static void wait_kbdc(void)         { while (inpb(0x64) & 2) { io_delay(); } }
 static void discard_kbdc_data(void) { while ((inpb(0x64) & 1)) { io_delay(); (void)inpb(0x60); } }
 
 static _Bool enable_a20_int15h(void)
@@ -30,44 +30,43 @@ static void enable_a20_kbdc(void)
   discard_kbdc_data();              // discard output data.
 
   wait_kbdc(); outpb(0x64, 0xd1);   // write output port.
-  wait_kbdc(); outpb(0x60, 0xdf);   // a20 on! (system reset too?!)
+  wait_kbdc(); outpb(0x60, 0xdf);   // a20 on! (FIXME: system reset too?!)
   wait_kbdc(); outpb(0x64, 0xae);   // turn kbd back on.
   wait_kbdc();
 }
 
 static _Bool check_a20(void)
 {
-  _u8 tmp, hi;
+  _u8 orig, lo;
+  _Bool r = true;
 
   // it's ok to pollute fs and gs...
   set_fs(0);
   set_gs(0xffff);
 
-  tmp = rd_fs8(0x500);
-  if (tmp != (hi = rd_gs8(0x510)))
-    return true;
-  wr_gs8(0x510, ~hi);
-  if (rd_fs8(0x500) == hi)
-  {
-    wr_fs8(0x500, tmp);
-    return false;
-  }
-  wr_fs8(0x500, tmp);
-  return true;    
+  orig = rd_fs8(0x500); // read 0:0x500.
+  wr_gs8(0x510, ~orig); // write its inverse in 0xffff:0x510
+  lo = rd_fs8(0x500);   // read 0:0x500 again.
+  if (orig == lo)       // if equal, a20 not enabled!
+    r = false;
+  wr_fs8(0x500, orig);  // puts 0:0x500 original value back.
+  return r;    
 }
 
 _Bool enable_a20(void)
 {
   _Bool r = true;
 
-  if (enable_a20_int15h())
-    return true;
-
-  enable_a20_fast();
-  if (!check_a20())
+  if (!enable_a20_int15h()) // if int 0x15 fails...
   {
-    enable_a20_kbdc();
-    r = check_a20();
+    /* FIXME: Should we test fast first? */
+
+    enable_a20_kbdc();      // try legacy code.
+    if (!check_a20())       // a20 not set?
+    {
+      enable_a20_fast();    // try fast code.
+      r = check_a20();      // test a20 again.
+    }
   }
 
   return r;
